dc736dc31b5b1062c471ceb3a2539e6272f49418
Nelli Butenko <butenkonelly93@gmail.com>
2021-10-27 11:38:07 -0400

    add basic style to nevigation section

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -5,13 +5,16 @@ const ContactsList = () => {
   return(
     <>
     <h1>I am a list of Contacts:</h1>
-    
-    <section>
-      <form>
+
+    <section className="contacts-navigation">
+      <form className="search-form">
         <label >Search: </label>
         <input type="text" placeholder="Type here to search"></input>
       </form>
+      <button className="add-contact-btn"> Add new contact </button>
     </section>
+
+
 
     <section className="contact-list">
       <ContactCard />
