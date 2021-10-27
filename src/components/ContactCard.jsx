66dbb55145e2c3bcb0e93409614dcd0b4f6845d4
Nelli Butenko <butenkonelly93@gmail.com>
2021-10-27 15:16:24 -0400

    list contact info in card

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,7 +3,14 @@ import "./ContactCard.scss";
 const ContactCard = () => {
   return (
     <div className="contact-card">
-      I am a contact card!
+      <img className="contact-photo" alt="Contact photo" src="https://randomuser.me/api/portraits/women/26.jpg"></img>
+
+      <ul className="contact-info">
+        <li><h4 className="name">Name</h4></li>
+        <li className="phone-number">12345678</li>
+        <li>New York</li>
+      </ul>
+
     </div>
   )
 }
