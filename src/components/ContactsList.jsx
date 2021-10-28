90ee6fd8b5b9ea890f38a5182fd1cd31b5af3c10
Nelli Butenko <butenkonelly93@gmail.com>
2021-10-28 13:28:50 -0400

    add hardcoded data and contacts state, pass down as props

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,7 +1,44 @@
+import { useState } from 'react';
+
 import ContactCard from "./ContactCard";
 import "./ContactsList.scss";
 
 const ContactsList = () => {
+
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: "Mary",
+      phone: "123-456-735",
+      image: "https://randomuser.me/api/portraits/women/26.jpg"
+    },
+    {
+      id: 2,
+      name: "Bob",
+      phone: "783-226-885",
+      image: "https://randomuser.me/api/portraits/women/26.jpg"
+    },
+    {
+      id: 3,
+      name: "John",
+      phone: "903-216-537",
+      image: "https://randomuser.me/api/portraits/women/26.jpg"
+    },
+  ]);
+
+  const contactsParsed = contacts.map(contact => {
+    return (
+      <ContactCard 
+        key={contact.id}
+        image={contact.image}
+        name={contact.name}
+        phone={contact.phone}
+      />
+    )
+  })
+
+  const display = contacts.length === 0 ? <h3>Your phone book is empty.</h3> : contactsParsed;
+
   return(
     <>
     <section className="contacts-navigation">
@@ -12,14 +49,7 @@ const ContactsList = () => {
       <button className="add-contact-btn"> Add contact </button>
     </section>
 
-    <section className="contact-list">
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-    </section>
+    <section className="contact-list"> {display} </section>
     </>
   )
 }
