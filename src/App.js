22c9da0dfee69c1b406aedc6ceb4023dbaf180b2
Nelli Butenko <butenkonelly93@gmail.com>
2021-10-30 12:56:43 -0400

    move form and navigation to upper level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 //components
+import ContactsNavigation from './components/ContactsNavigation';
+import NewContactForm from './components/NewContactForm';
 import ContactsList from './components/ContactsList';
 //styles
 import './App.scss';
 
+
+
 function App() {
+
+  const [form, setForm] = useState(false);
+  const showForm = () => {
+    setForm(true);
+  }
   return (
     <div className="app">
       <h1>My Phone Book</h1>
+      <ContactsNavigation showForm={showForm} />
+      {form && <NewContactForm />}
       <ContactsList />
     </div>
   );
