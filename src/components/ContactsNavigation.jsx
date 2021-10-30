11b5baec710bec3f6bfba77b275705fb4021cf27
Nelli Butenko <butenkonelly93@gmail.com>
2021-10-30 12:52:05 -0400

    separate navigation section into a component

---
+++ src/components/ContactsNavigation.jsx
@@ -0,0 +1,18 @@
+import './ContactsNavigation.scss'
+
+const ContactsNavigation = ({showForm}) => {
+
+  return (
+    <section className="contacts-navigation">
+      <form className="search-form">
+        <div className="label-input-div">
+          <label >Search: </label>
+          <input className="search-input" type="text" placeholder="Type here to search"></input>
+        </div>
+      </form>
+      <button type="button" onClick={showForm} class="btn btn-warning">New Contact</button>
+    </section>
+  )
+}
+
+export default ContactsNavigation;
