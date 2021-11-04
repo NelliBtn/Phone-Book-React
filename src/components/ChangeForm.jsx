e357cf79a048dae6049ece13cb7577674abcd229
Nelli Butenko <butenkonelly93@gmail.com>
2021-11-04 15:48:22 -0400

    implement changeContact function for each input field

--- src/components/ChangeForm.jsx
+++ src/components/ChangeForm.jsx
@@ -1,26 +1,19 @@
-const ChangeForm = ({contact, setInitContact}) => {
+const ChangeForm = ({contact, changeContact}) => {
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setInitContact({
-      ...contact,
-      [e.target.name]: e.target.value
-    })
-  }
 
   return (
     <div className="change-form">
        <div className='label-input-div'>
           <label>Name:</label>
-          <input type='text' name='name' value={contact.name} onChange={handleChange}></input>
+          <input type='text' name='name' value={contact.name} onChange={(e) => changeContact(e, contact.id)}></input>
         </div>
         <div className='label-input-div'>
           <label>Phone:</label>
-        <input type='tel' name='phone' value={contact.phone} onChange={handleChange}></input>
+        <input type='tel' name='phone' value={contact.phone} onChange={(e) => changeContact(e, contact.id)}></input>
         </div>
         <div className='label-input-div'>
           <label>Custom:</label>
-          <input type='text' name='custom' value={contact.custom} onChange={handleChange}></input>
+        <input type='text' name='custom' value={contact.custom} onChange={(e) => changeContact(e, contact.id)}></input>
         </div>
         <br/>
         <button type="submit" className="btn btn-warning">Change</button>
