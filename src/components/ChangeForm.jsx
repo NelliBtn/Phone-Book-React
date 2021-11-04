dee5fa8d2abf3480c5ad4e5622e0ecf787eb80a8
Nelli Butenko <butenkonelly93@gmail.com>
2021-11-04 16:01:55 -0400

    rename btn to save

--- src/components/ChangeForm.jsx
+++ src/components/ChangeForm.jsx
@@ -3,6 +3,7 @@ const ChangeForm = ({contact, changeContact}) => {
 
   return (
     <div className="change-form">
+      <h4>Edit contact</h4>
        <div className='label-input-div'>
           <label>Name:</label>
           <input type='text' name='name' value={contact.name} onChange={(e) => changeContact(e, contact.id)}></input>
@@ -16,7 +17,7 @@ const ChangeForm = ({contact, changeContact}) => {
         <input type='text' name='custom' value={contact.custom} onChange={(e) => changeContact(e, contact.id)}></input>
         </div>
         <br/>
-        <button type="submit" className="btn btn-warning">Change</button>
+        <button type="submit" className="btn btn-warning">Save</button>
 
     </div>
   )
