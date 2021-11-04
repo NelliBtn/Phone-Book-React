e5bfcf354a65a0262e83a4c41c29f439dcaaba7d
Nelli Butenko <butenkonelly93@gmail.com>
2021-11-04 12:42:38 -0400

    pass down 'custom' as prop and change 'contact' to 'initContact' to avoid confusion

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -3,7 +3,7 @@ import ContactCard from "./ContactCard";
 import "./ContactsList.scss";
 
 
-const ContactsList = ({contacts, onDelete}) => {
+const ContactsList = ({contacts, onDelete, setInitContact}) => {
 
   const contactsParsed = contacts.map(contact => {
     return (
@@ -13,7 +13,10 @@ const ContactsList = ({contacts, onDelete}) => {
         image={contact.image}
         name={contact.name}
         phone={contact.phone}
+        custom={contact.custom}
         onDelete={onDelete}
+        setInitContact={setInitContact}
+        contact={contact}
       />
     )
   })
