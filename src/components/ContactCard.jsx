81e6777cf75a48347fdd988d5e670d3eb9ed581e
Nelli Butenko <butenkonelly93@gmail.com>
2021-11-04 12:44:12 -0400

    add "change form" conditional render and logic

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,20 +1,36 @@
+import { useState } from 'react';
+import ChangeForm from './ChangeForm'
 import "./ContactCard.scss";
 
-const ContactCard = ({name, image, phone, id, onDelete }) => {
+const ContactCard = ({name, image, phone, id, custom, onDelete, contact, setInitContact }) => {
+
+  const [changeForm, setChangeForm] = useState(false);
+  const handleChange = () => {
+    // e.preventDefault();
+    setChangeForm(true);
+  }
 
   return (
     <div className="contact-card">
-      <img className="contact-photo" alt="Contact photo" src={image}></img>
+      {changeForm && (
+        <ChangeForm contact={contact} setInitContact={setInitContact}/>
+      )}
+      {!changeForm && (
+        <>
+        <img className="contact-photo" alt="Contact photo" src={image}></img>
+  
+        <ul className="contact-info">
+          <li><h4 className="name">{name}</h4></li>
+          <li className="phone-number">{phone}</li>
+            <li className="custom">{custom}</li>
+        </ul>
+  
+        <button type='submit' onClick={handleChange}>Change</button>
+        <button type='submit' onClick={(e) => onDelete(e, id)}>Delete</button>
+        <button>History</button>
+        </>
 
-      <ul className="contact-info">
-        <li><h4 className="name">{name}</h4></li>
-        <li className="phone-number">{phone}</li>
-        <li>New York</li>
-      </ul>
-
-      <button>Change</button>
-      <button type='submit' onClick={(e) => onDelete(e, id)}>Delete</button>
-      <button>History</button>
+      )}
 
     </div>
   )
